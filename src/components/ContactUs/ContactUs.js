fb0bcea5daa8fee605d67d7adcfcd3cb6fa1204c
Zaki Al Faridzi <44135822+zakialfaridzi@users.noreply.github.com>
2021-01-14 11:03:04 +0700

    fixed texts

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -13,7 +13,7 @@ const subContainerStyle = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
 const subButtonStyle = `
       border-radius: 5px;
       flex-grow: 1;
-      background-color: #0000;
+      background-color: #fdfdfd;
       color: #333;
 
       &:hover{
@@ -51,7 +51,7 @@ export default ({
       <span style={{ color: "#333" }}>
         Yuk Subscribe{" "}
         <span tw="text-primary-500" id="closedbeta">
-          Newsletter
+          newz
         </span>
         <wbr /> Kami.
       </span>
